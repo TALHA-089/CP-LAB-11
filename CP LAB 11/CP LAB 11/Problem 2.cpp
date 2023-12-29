e641eae291e12d09034704a330ffbb1360e3cfb1
Raja Talha <146324114+TALHA-089@users.noreply.github.com>
2023-12-29 23:44:27 +0500

    Add files via upload

---
+++ CP LAB 11/CP LAB 11/Problem 2.cpp
@@ -0,0 +1,33 @@
+//#include<iostream>
+//using namespace std;
+//
+//struct Phone
+//{
+//	int area_code;
+//	int exchange;
+//	int number;
+//};
+//
+//int main()
+//{
+//	Phone My_number;
+//	Phone Your_number;
+//
+//	My_number.area_code = 456;
+//	My_number.exchange = 746;
+//	My_number.number = 8099;
+//
+//	cout << "\n\nEnter your area code,exchange, and number: ";
+//	cin >> Your_number.area_code;
+//	cin >> Your_number.exchange;
+//	cin >> Your_number.number;
+//
+//	cout << "\n\nMy Number is (" << My_number.area_code << ") " << My_number.exchange << "-" << My_number.number;
+//	cout << "\n\nYour Number is (" << Your_number.area_code << ") " << Your_number.exchange << "-" << Your_number.number;
+//
+//
+//
+//	cout << endl << endl;
+//	system("pause");
+//	return 0;
+//}
